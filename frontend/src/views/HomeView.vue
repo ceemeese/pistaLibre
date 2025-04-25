bbdd1a049d8385e63ed399852f7914983a945fe6
ceemeese <cmsalvador92@gmail.com>
2025-04-25 17:42:10 +0200

    Componentes Resume y CourtSchedule

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <template>
     <Banner @scrollToDatepicker="scrollToDatepicker"/>
     <v-container>
+      <Stepper />
       <Datepicker ref="datepickerRef" />
       <ListCourt />
+      <Resume
+      
+      :pista="pistaSeleccionada || 'Pista 1'"
+      :reserva="reserva"
+      @confirmar="confirmarReserva"
+      @cancelar="cancelarReserva"
+    />
     </v-container>
 </template>
 
@@ -11,6 +19,8 @@
   import Banner from '../components/AppBanner.vue'
   import Datepicker from '@/components/AppDatepicker.vue';
   import ListCourt from '@/components/AppListCourt.vue';
+  import Resume from '../components/AppResume.vue';
+
   import { ref } from 'vue'
 
   const datepickerRef = ref<HTMLElement | null>(null);
@@ -18,5 +28,28 @@
   function scrollToDatepicker() {
     datepickerRef.value?.scrollTo();
   }
+
+  const pistaSeleccionada = ref<string | null>('Pista 1')
+  const reserva = ref({
+    fecha: '2025-04-24',  
+    hora: '18:00'       
+  })
+
+  const seleccionarPista = (pista: string) => {
+  pistaSeleccionada.value = pista
+  }
+
+const confirmarReserva = (cliente: { nombre: string, email: string }) => {
+  console.log('Reserva confirmada:', {
+    pista: pistaSeleccionada.value,
+    ...reserva.value,
+    ...cliente
+  })
+  pistaSeleccionada.value = null
+}
+
+const cancelarReserva = () => {
+  pistaSeleccionada.value = null
+}
 
 </script>
