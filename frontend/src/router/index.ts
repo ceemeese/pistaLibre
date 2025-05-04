558eb70af1d6bab4ba55a1502d0848955b9d6cad
ceemeese <cmsalvador92@gmail.com>
2025-05-04 07:47:17 +0200

    Add vista reservas en admin

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -30,6 +30,14 @@ const router = createRouter({
       component: () => import('../views/CourtsView.vue'),
     },
     {
+      path: '/admin/reservations',
+      name: 'reservations',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ReservationsView.vue'),
+    },
+    {
       path: '/user',
       name: 'user',
       component: UserView,
---
+++ frontend/src/views/ReservationsView.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container>
+        <h1 class="text-h4 text-md-h3 mb-6 mb-md-10 text-center mt-3 mt-md-10">Reservas</h1>
+        <AppDateTable
+            :headers="headers"
+            :items="store.reservations"
+        />
+    </v-container>
+</template>
+
+<script setup lang="ts">
+
+    import { onMounted } from 'vue'
+    import { useReservationsStore } from '@/stores/reservationStore'
+    import AppDateTable from '@/components/AppDateTable.vue'
+
+
+    const store = useReservationsStore();
+
+    const headers = [
+        { text: 'ID', value: 'id' },
+        { text: 'Pista', value: 'courtId' },
+        { text: 'Usuario', value: 'userId' },
+        { text: 'Fecha', value: 'date' },
+        { text: 'Hora inicio', value: 'startTime' },
+        { text: 'Hora fin', value: 'endTime' },
+    ]
+
+
+    onMounted(() => {
+        store.fetchAll()
+    })
+</script>
+
+
+<style scoped>
+</style>
